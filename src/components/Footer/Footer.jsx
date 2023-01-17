37ed26eee69ae21848ee2f197de4191f06ff8d5d
= <iarlensr@hotmail.com>
2023-01-17 16:45:08 -0300

    Adicioando projeto no github

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <h3>Compartilhe momentos e experiências!</h3>
+        <p>Life Shared &copy; 2023</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
